4a4773cfda88ee5d2cbee15e2aa97fb9c0e463dd
lks <82043888+lks1337@users.noreply.github.com>
2021-07-26 00:51:16 -0400

    Update all-code.js

--- all-code.js
+++ all-code.js
@@ -13,7 +13,7 @@ client.login(settings.token).catch(err => console.log(err)); // connexion du bot
 
 client.on('ready', () => {console.clear(), console.log(` ${client.user.username} (https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)`)})
 
-client.on('message', message => {
+client.on('message', async(message) => {
     if(message.content === settings.prefix + 'admins') {
     if(!message.member.permissions.has('ADMINISTRATOR')) return; // vérifie si l'utilisateur a les perms admins
     let admins = message.guild.members.cache.filter(m => m.permissions.has('ADMINISTRATOR')) // fait une liste des membres admins
@@ -32,5 +32,19 @@ client.on('message', message => {
         .setColor(settings.maincolor)
         .setFooter(`Compteur: ${bots.size}`);
         message.channel.send(Embed); // envoie l'embed
-        }
+        }else if([settings.prefix + 'banlist', settings.prefix + 'bans'].includes(message.content)) {
+            if(!message.member.permissions.has('ADMINISTRATOR')) return; // vérifie si l'utilisateur a les perms admins
+            const fetchBans = message.guild.fetchBans();
+            const bannedMembersSize = (await fetchBans)
+            .map(u => u).length
+            const bannedMembers = (await fetchBans)
+            .map((member) => `\`${member.user.id}\` : **${member.user.username}**`)
+            .join("\n") 
+            let Embed = new Discord.MessageEmbed()
+            .setTitle('Liste des bots')
+            .setDescription(bannedMembers ? bannedMembers : `Personne n'est banni sur **${message.guild.name}** !`)
+            .setColor(settings.maincolor)
+            .setFooter(`Compteur: ${bannedMembersSize}`);
+            message.channel.send(Embed); // envoie l'embed
+            }
 })
