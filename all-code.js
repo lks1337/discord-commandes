cacc45bec3d01c6c1eeafe913a0b5bcab25b8ce3
lks <82043888+lks1337@users.noreply.github.com>
2021-07-26 00:34:29 -0400

    Update all-code.js

--- all-code.js
+++ all-code.js
@@ -1,7 +1,7 @@
         // #CONFIGURATION
         const settings = {
             token: "", // le token du bot
-            prefix: "+", // le prefix du bot
+            prefix: ".", // le prefix du bot
             maincolor: "#303136"
         }
         // #MODULES
@@ -23,5 +23,14 @@ client.on('message', message => {
     .setColor(settings.maincolor)
     .setFooter(`Compteur: ${admins.size}`);
     message.channel.send(Embed); // envoie l'embed
-    }
+    }else if(message.content === settings.prefix + 'bots') {
+        if(!message.member.permissions.has('ADMINISTRATOR')) return; // vérifie si l'utilisateur a les perms admins
+        let bots = message.guild.members.cache.filter(m => m.user.bot) // fait une liste des membres bots
+        let Embed = new Discord.MessageEmbed()
+        .setTitle('Liste des bots')
+        .setDescription(bots.map(m => `\`${m.id}\` : ${m}`))
+        .setColor(settings.maincolor)
+        .setFooter(`Compteur: ${bots.size}`);
+        message.channel.send(Embed); // envoie l'embed
+        }
 })
