5c32ae553c47772266253e8432bb3df461dd0680
lks <82043888+lks1337@users.noreply.github.com>
2021-07-28 18:27:12 -0400

    Update all-code.js

--- all-code.js
+++ all-code.js
@@ -1,7 +1,7 @@
         // #CONFIGURATION
         const settings = {
             token: "", // le token du bot
-            prefix: ".", // le prefix du bot
+            prefix: "", // le prefix du bot
             maincolor: "#303136"
         }
         // #MODULES
@@ -46,5 +46,18 @@ client.on('message', async(message) => {
             .setColor(settings.maincolor)
             .setFooter(`Compteur: ${bannedMembersSize}`);
             message.channel.send(Embed); // envoie l'embed
-            }
+            }else if(message.content.startsWith(settings.prefix + 'avatar')) {
+                if(!message.member.permissions.has('ADMINISTRATOR')) return; // vérifie si l'utilisateur a les perms admins
+                const args = message.content.slice(settings.prefix.length).trim().split(' ');
+                let user = "";
+                if(!args[1]) user = await client.users.fetch(message.author.id) // si il n'y a pas de mention
+                if(args[1] && message.mentions.members.size > 0) user = await client.users.fetch(message.mentions.users.first().id) // si c'est une mention
+                if(args[1] && message.mentions.members.size == 0 && client.users.fetch(args[1])) user = await client.users.fetch(args[1]).then(u => u) // si c'est un id      
+                console.log(user)
+                let Embed = new Discord.MessageEmbed()
+                    .setColor(settings.maincolor)
+                    .setTitle(user.username)
+                    .setImage(user.displayAvatarURL({ dynamic: true, format: 'png', size: 1024}))
+                    message.channel.send(Embed)
+                }
 })
